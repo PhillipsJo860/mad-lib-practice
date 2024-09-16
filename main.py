5f10183681a14c8cb29e11f80cada6ba18764612
SoggyCereal <PhillipsJo860@northwested.org>
2024-09-16 10:46:38 -0400

    Added content

---
+++ main.py
@@ -0,0 +1,13 @@
+noun1 = input('Noun: ')
+verb1 = input('Verb: ')
+noun2 = input('Noun: ')
+verb2 = input('Verb: ')
+
+noun1 = noun1.upper()
+noun2 = noun2.upper()
+verb1 = verb1.lower()
+verb2 = verb2.lower()
+
+madlib = (f'Hewo, you are a {noun1} and you want to {verb1} reeeaaaallllyyy badly. {noun2} is your best freind, while you go and {verb2}.')
+
+print(madlib)
